916cd8f7fb9a9782d3e0e80ffac73b5b5d2e401a
Kayke Denelle <kaykedenelle@gmail.com>
2024-12-02 19:11:02 -0300

    1

--- models/deck.js
+++ models/deck.js
@@ -1,14 +1,20 @@
-// models/deck.js
 const mongoose = require('mongoose');
 
 const deckSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
   // Outros campos existentes
   revisionDates: [Date], // Array para armazenar as datas de revisão
 });
 
-const Deck = mongoose.model('Deck', deckSchema);
+module.exports = mongoose.model('Deck', deckSchema);
 module.exports = Deck;
+
+
