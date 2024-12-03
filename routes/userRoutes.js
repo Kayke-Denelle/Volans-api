144baa0a24ca04599b57fd35019fdac6d6a1809f
Kayke Denelle <kaykedenelle@gmail.com>
2024-12-02 23:05:55 -0300

    1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user'); // Modelo do usuário
+const Deck = require('../models/deck')
 require('dotenv').config();
 
 const router = express.Router();
