8eaf5828b736a686c8e54c96c813549ef7821437
Kayke Denelle <kaykedenelle@gmail.com>
2024-12-02 21:33:25 -0300

    1

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-  const express = require('express');
-  const mongoose = require('mongoose');
-  const cors = require('cors');
-  const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-  dotenv.config();
+dotenv.config();
 
-  const authRoutes = require('./routes/authRoutes');
-  const deckRoutes = require('./routes/deckRoutes');
-  const cardRoutes = require('./routes/cardsRoutes');
-  const revisionRoutes = require('./routes/revisionRoutes')
+const authRoutes = require('./routes/authRoutes');
+const deckRoutes = require('./routes/deckRoutes');
+const cardRoutes = require('./routes/cardsRoutes');
 
 
-  const app = express();
+const app = express();
 
-  // Middleware
-  app.use(express.json());
-  app.use(cors());
+// Middleware
+app.use(express.json());
+app.use(cors());
 
-  app.get("/", (req, res) => {
-    return res.json("Funcionou");
-  });
+app.get("/", (req, res) => {
+  return res.json("Funcionou");
+});
 
-  // Routes
-  app.use('/api/auth', authRoutes);
-  app.use('/api/baralhos', deckRoutes);
-  app.use('/api/cartas', cardRoutes);
-  app.use('/api/revisions', revisionRoutes);
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/baralhos', deckRoutes);
+app.use('/api/cartas', cardRoutes);
 
-  // Conexão com MongoDB
-  mongoose.connect(process.env.MONGO_URI, )
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log('MongoDB connection error: ', err));
+// Conexão com MongoDB
+mongoose.connect(process.env.MONGO_URI, )
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error: ', err));
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
